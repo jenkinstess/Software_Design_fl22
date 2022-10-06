1f11d9e74cad86afe33d688598f0f531e636acaf
tessjenkins <jenkinstess@wustl.edu>
2022-10-06 13:11:05 -0500

    fs error. idk

--- ticket_selling/next.config.js
+++ ticket_selling/next.config.js
@@ -5,3 +5,16 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// module.exports = (phase, { defaultConfig }) => {
+//   return {
+//     ...defaultConfig,
+
+//     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//       config.node = {
+//         fs: 'empty'
+//       }
+//       return config
+//     },
+//   }
+// }
