646633ca65a01b6d6ab73284362d29390f32c2f0
tessjenkins <jenkinstess@wustl.edu>
2022-10-13 23:32:00 -0500

    sdf

--- ticket_selling/pages/index.js
+++ ticket_selling/pages/index.js
@@ -10,43 +10,43 @@ const db = require('/config/database');
 //     .catch(err => console.log("Error: " + err))
 
  function Home() {
-//   const {data, revalidate} = useSWR('/api/me', async function(args) {
-//     const res = await fetch(args);
-//     return res.json();
-//   });
-//   if (!data) return <h1>Loading...</h1>;
-//   let loggedIn = false;
-//   if (data.email) {
-//     loggedIn = true;
-//   }
-//   return (
-//     <div>
-//       <Head>
-//         <title>Welcome to landing page</title>
-//         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-//       </Head>
-//       <h1>PARTY TICKETS</h1>
-//       {loggedIn && (
-//         <>
-//           <p>Welcome {data.email}!</p>
-//           <button
-//             onClick={() => {
-//               cookie.remove('token');
-//               revalidate();
-//             }}>
-//             Logout
-//           </button>
-//         </>
-//       )}
-//       {!loggedIn && (
-//         <>
-//           <Link href="/login">Login</Link>
-//           <p>or</p>
-//           <Link href="/signup">Sign Up</Link>
-//         </>
-//       )}
-//     </div>
-//   );
+  const {data, revalidate} = useSWR('/api/me', async function(args) {
+    const res = await fetch(args);
+    return res.json();
+  });
+  if (!data) return <h1>Loading...</h1>;
+  let loggedIn = false;
+  if (data.email) {
+    loggedIn = true;
+  }
+  return (
+    <div>
+      <Head>
+        <title>Welcome to landing page</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>PARTY TICKETS</h1>
+      {loggedIn && (
+        <>
+          <p>Welcome {data.email}!</p>
+          <button
+            onClick={() => {
+              cookie.remove('token');
+              revalidate();
+            }}>
+            Logout
+          </button>
+        </>
+      )}
+      {!loggedIn && (
+        <>
+          <Link href="/login">Login</Link>
+          <p>or</p>
+          <Link href="/signup">Sign Up</Link>
+        </>
+      )}
+    </div>
+  );
  }
 
 export default Home;
