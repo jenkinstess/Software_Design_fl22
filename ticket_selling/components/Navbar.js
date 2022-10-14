b04d3d51e050613bcd0f785c965320e782c56bd9
Ina Li <ili@wustl.edu>
2022-10-14 10:19:59 -0500

    poo2

--- ticket_selling/components/Navbar.js
+++ ticket_selling/components/Navbar.js
@@ -1,30 +1,33 @@
 import React, {Component} from 'react'
 // import styles from "../styles/Navbar.css";
 
-export default function Navbar(){    
-    return <>
+function Navbar(){    
+    return (
         <div className = "Navbar">
                 
-            <a href = "/">Home</a>
+            <Link href = "/">
+                <a>Home</a>
+            </Link>   
             {console.log(localStorage.getItem("loginoutcome") === "true")}
             {localStorage.getItem("loginoutcome") === "true" ? (
                 <>
-                <a href = "/recommendations">Recommendations</a>
-                <a href = "/profile">Profile</a>
-                <a href = "/savedPackages">Saved Packages</a>
-                <a href = "/" onClick={this.logOut.bind()}>Log Out</a>
+                <Link href = "/profile"><a>Profile</a></Link> 
+                <Link href = "/buy"><a>Buy</a></Link> 
+                <Link href = "/sell"><a>Sell</a></Link> 
+                {/* <a href = "/" onClick={this.logOut.bind()}>Log Out</a> */}
                 </>
             ) : (
       
                 <>
-                <a href = "/login" >Login</a>
-                <a href = "/register">Register</a>
+                <Link href = "/login"><a>Login</a></Link>
+                <Link href = "/signup"><a>Sign Up</a></Link>  
             </>
                 
             )}
         </div>
-    </>
+    )
 }
+export default Home
 
 
 // export default function Layout(children){
