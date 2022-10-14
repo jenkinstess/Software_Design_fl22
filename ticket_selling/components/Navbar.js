115226cd883420c5c773229eb313be9e3c2e4819
Ina Li <ili@wustl.edu>
2022-10-14 11:07:18 -0500

    poop4

--- ticket_selling/components/Navbar.js
+++ ticket_selling/components/Navbar.js
@@ -1,35 +1,36 @@
-import React, {Component} from 'react'
-import Link from 'next/link';
-import { useEffect } from 'react';
-// import styles from "../styles/Navbar.css";
+// import React, {Component} from 'react'
+// import Link from 'next/link';
+// import { useEffect } from 'react';
+// // import styles from "../styles/Navbar.css";
 
-export default function Navbar(){ 
-    useEffect(() => {
-        const loginoutcome = localStorage.getItem("loginoutcome")
-    }, [])
+// export default function Navbar(){ 
+//     this.state = {
+//         loginoutcome: true
+//     };
+    
        
-    return (
-        <div className = "Navbar">
+//     return (
+//         <div className = "Navbar">
                 
-            <Link href = "/">
-                <a>Home</a>
-            </Link>   
-            {/* {console.log(localStorage.getItem("loginoutcome") === "true")} */}
-            {/* {localStorage.getItem("loginoutcome") === "true" ? ( */}
+//             <Link href = "/">
+//                 <a>Home</a>
+//             </Link>   
+//             {/* {console.log(localStorage.getItem("loginoutcome") === "true")} */}
+//             {/* {localStorage.getItem("loginoutcome") === "true" ? ( */}
                 
-            {localStorage.getItem("loginoutcome") === "true" ? (
-                <>
-                <Link href = "/profile"><a>Profile</a></Link> 
-                <Link href = "/buy"><a>Buy</a></Link> 
-                <Link href = "/sell"><a>Sell</a></Link> 
-                </>
-            ):(
+//             {localStorage.getItem("loginoutcome") === "true" ? (
+//                 <>
+//                 <Link href = "/profile"><a>Profile</a></Link> 
+//                 <Link href = "/buy"><a>Buy</a></Link> 
+//                 <Link href = "/sell"><a>Sell</a></Link> 
+//                 </>
+//             ):(
         
-                <>
-                <Link href = "/login"><a>Login</a></Link>
-                <Link href = "/signup"><a>Sign Up</a></Link>  
-                </>
-            )}
-        </div>
-    )
-}
+//                 <>
+//                 <Link href = "/login"><a>Login</a></Link>
+//                 <Link href = "/signup"><a>Sign Up</a></Link>  
+//                 </>
+//             )}
+//         </div>
+//     )
+// }
