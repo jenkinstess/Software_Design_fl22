ec5d2848cfbbab1667c956301ada7b327198b22a
tessjenkins <jenkinstess@wustl.edu>
2022-10-17 15:36:50 -0500

    still registration errors.

--- ticket_selling/pages/api/users.js
+++ ticket_selling/pages/api/users.js
@@ -19,6 +19,9 @@ const dbName = 'ticketsdb';
 const sequelize = new Sequelize('ticketsitedb', 'ticketgroup', 'partytixstinks',{
   host: 'ticket-site-db.cvddhqhvjcur.us-east-1.rds.amazonaws.com', 
   dialect: 'mysql',
+  define:{
+    timestamps: false
+  },
   // dialectModule:'mysql2',
   operatorsAliases: false,
 
@@ -33,46 +36,56 @@ const sequelize = new Sequelize('ticketsitedb', 'ticketgroup', 'partytixstinks',
 const users = sequelize.define('users', {
   userid:{
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
+    primaryKey: true
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false
   },
   password:{
-    tyoe: DataTypes.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   }
 });
 
+users.sync().then(
+  () => console.log("initial sync complete")
+);
 
-function findUser(email, callback) {
-  const found = users.findAll({
-    where:{
-      email: email
-    },
-    function(err, found){
-      assert.strictEqual(err, null);
-      callback(userFound);
-    }
-  });
-}
+// function findUser(email, callback) {
+//   console.log("test beginning of find");
+//   const found = users.findAll({
+//     where:{
+//       email: email
+//     },
+//     function(err, found){
+//       assert.strictEqual(err, null);
+//       callback(found);
+//     }
+//   });
+//   console.log("test after find");
+// }
 
-function createUser(email, password, callback) {
-
+// function createUser(email, password, callback) {
+  function createUser(email, password) {
+  console.log("test before create hash");
   bcrypt.hash(password, round, function(err, hash) {
+    console.log("test within create hash");
     console.log(hash)
     // Stores the hash in the password db
     const newUser = users.create({
       userid: v4(),
       email: email,
       password: hash,
-      function(err, userCreated) {
-        assert.strictEqual(err, null);
-        callback(userCreated);
-      }
+      // function(err, userCreated) {
+      //   assert.strictEqual(err, null);
+      //   callback(userCreated);
+      // }
     });
   })
+  console.log("test after create hash");
+
 }
 
 export default (req, res) => {
@@ -98,36 +111,70 @@ export default (req, res) => {
         console.log("data grabbed");
         console.log(password);
 
-        findUser(email, function(err, user) {
-          if(err) {
-            res.status(500).json({error: true, message: 'Error finding user'});
-            return;
+        const found = users.findAll({
+          where:{
+            email: email
           }
-          if(!user){
-            // create a user
-            createUser(email, password, function(creationResult) {
-              if(creationResult.ops.length == 1) {
-                const user = creationResult.ops[0];
-                const token = jwt.sign(
-                  {userid: user.userid, email: user.email},
-                  jwtSecret,
-                  {
-                    expiresIn: 3000, //50 mins
-                  },
-                );
-                res.status(200).json({token});
-                return;
-              }
-            });
-          }
-          else {
+        }).then(function(found){
+          return found;
+        });
+
+        console.log("found filled");
+        console.log(found);
+        
+        if(found == []){
+          console.log("found nothing");
+          createUser(email, password);
+          res.status(200).json({token});
+          return;
+        }
+        else{
             // user already exists
+            console.log("found user");
             res.status(403).json({error: true, message: 'email exists'});
             return;
-          }
-        });
+        }
+
+        // findUser(email, function(err, user) {
+        //   console.log("beginning here!!!");
+        //   if(err) {
+        //     console.log("error finding user");
+        //     res.status(500).json({error: true, message: 'Error finding user'});
+        //     return;
+        //   }
+        //   console.log("before if user");
+        //   if(!user){
+        //     console.log("after if user");
+        //     // create a user
+        //     createUser(email, password, function(creationResult) {
+        //       console.log("before creation result call");
+        //       if(creationResult.ops.length == 1) {
+        //         console.log("before creation result call");
+        //         const user1 = creationResult.ops[0];
+        //         //const token = {userid: user1.userid, email: user1.email, password: user1.password};
+        //         const token = jwt.sign(
+        //           {userid: user1.userid, email: user1.email, password: user1.password},
+        //           jwtSecret,
+        //           {
+        //             expiresIn: 3000, //50 mins
+        //           },
+        //         );
+        //         res.status(200).json({token});
+        //         return;
+        //       }
+        //     });
+        //   }
+        //   else {
+        //     // user already exists
+        //     res.status(403).json({error: true, message: 'email exists'});
+        //     return;
+        //   }
+        // });
       }).catch((error) => {
         console.error ('unable to connect to the db: ', error);
       });
+      users.sync().then(
+        () => console.log("final sync complete")
+      );
     }
   };
