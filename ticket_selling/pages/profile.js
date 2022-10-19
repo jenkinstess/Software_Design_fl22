6591b8807fdd6b6da211b6836f3fe02e474f258b
jordanshonfeld <j.shonfeld@wustl.edu>
2022-10-18 20:48:43 -0500

    sequelize models

--- ticket_selling/pages/profile.js
+++ ticket_selling/pages/profile.js
@@ -8,18 +8,6 @@ function Profile() {
     const data = await response.json()
     setUsers(data)
   }
-
-  // export const getStaticProps = async () => {
-  //   // pulling events data from events-data.js file
-  //   const response = await fetch('/api/profile')
-  //   const user = await response.json()
-  //   return {
-  //       props: {
-  //           user,
-  //       },
-  //   }
-  // }
-
   return(
     <>
     <button onClick={fetchUsers}>Load Profile</button>
