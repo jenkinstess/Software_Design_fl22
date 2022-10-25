5a9d2a21dc84b887f667c5bad557e7ee85c30b98
leibmalina <leib.malina@wustl.edu>
2022-10-25 17:07:32 -0500

    event list component updated

--- ticket_selling/components/EventList.js
+++ ticket_selling/components/EventList.js
@@ -3,10 +3,12 @@ import eventStyles from '../styles/Event.module.css'
 
 
 const EventList = ({ events }) => {
+    console.log("events JSON:")
+    console.log(typeof events.result)
     return (
         // TODO: add filtering here for here for next week, day, etc. and sorting by event date
         <div className={eventStyles.list}>
-            {events.map((event) => (
+            {events && events.result && events.result.map((event) => (
                 <EventItem event={event} />
             ))}
         </div>
