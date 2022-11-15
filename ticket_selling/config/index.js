7397ac8f91f412bb23eaef47f29637b163751a87
jordanshonfeld <j.shonfeld@wustl.edu>
2022-11-15 15:56:54 -0600

    testing dev

--- ticket_selling/config/index.js
+++ ticket_selling/config/index.js
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
+console.log(dev)
 
 export const server = dev ? 'http://localhost:3000' : 'http://ec2-3-141-164-182.us-east-2.compute.amazonaws.com:3000';
