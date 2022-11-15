533ca85177dd62a114d314604c0a873cce707380
leibmalina <63120687+leibmalina@users.noreply.github.com>
2022-11-15 16:52:24 -0600

    updated event item UI

--- ticket_selling/components/EventItem.js
+++ ticket_selling/components/EventItem.js
@@ -3,13 +3,21 @@ import eventStyles from '../styles/Event.module.css'
 
 const EventItem = ({ event }) => {
     return (
-      <Link href={`/event/${event.id}`}>
-        <a className={eventStyles.preview}>
-          <h3>{event.name}</h3>
-          <h5>{event.date}</h5>
-          <p>{event.description} &rarr;</p>
-        </a>
-      </Link>
+      // <Link href={`/event/${event.id}`}>
+      //   <a className={eventStyles.preview}>
+      //     <h3>{event.name}</h3>
+      //     <h5>{event.date}</h5>
+      //     <p>{event.description} &rarr;</p>
+      //   </a>
+      // </Link>
+      <div class="card text-center mt-4 mx-auto">
+        <div class="card-body">
+          <h5 class="card-title">{event.name}</h5>
+          <p class="card-text"><i>Details:</i> {event.description}</p>
+          <a href={`/event/${event.id}`} class="btn btn-primary">View Tickets &rarr;</a>
+        </div>
+        <div class="card-footer text-muted"><i>Date:</i> {event.date}</div>
+      </div>
     )
   }
   
