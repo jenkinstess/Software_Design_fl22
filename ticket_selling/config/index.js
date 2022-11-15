1692524309988509e72cafe046af2ec8f23e2dfb
jordanshonfeld <j.shonfeld@wustl.edu>
2022-11-15 15:14:41 -0600

    fixing

--- ticket_selling/config/index.js
+++ ticket_selling/config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'http://ec2-3-141-164-182.us-east-2.compute.amazonaws.com:3000/';
+export const server = dev ? 'http://localhost:3000' : 'http://ec2-3-141-164-182.us-east-2.compute.amazonaws.com:3000';
