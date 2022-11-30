931c23d4db3acab35026971c05f1cdb61ec75be1
Ina Li <ili@wustl.edu>
2022-11-29 21:51:04 -0600

    test

--- ticket_selling/config/index.js
+++ ticket_selling/config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 console.log(dev)
 
-//export const server = dev ? 'http://localhost:3000' : 'http://ec2-3-15-187-114.us-east-2.compute.amazonaws.com:3001';
-export const server = 'http://ec2-3-15-187-114.us-east-2.compute.amazonaws.com:3000';
+export const server = dev ? 'http://localhost:3000' : 'http://ec2-3-15-187-114.us-east-2.compute.amazonaws.com:3001';
+//export const server = 'http://ec2-3-15-187-114.us-east-2.compute.amazonaws.com:3000';
